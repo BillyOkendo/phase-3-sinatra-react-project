776955b840d1f46edf74ac05cce47bdac1fbd45e
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-09-07 01:24:36 +0300

    modified migration file

--- db/migrate/20220906222120_create_roles.rb
+++ db/migrate/20220906222120_create_roles.rb
@@ -1,4 +1,10 @@
 class CreateRoles < ActiveRecord::Migration[6.1]
   def change
+    create_table :tasks do |t|
+      t.string :salary
+      t.string :username
+      t.integer :attendant_id
+      t.integer :house_id
+    end
   end
 end
