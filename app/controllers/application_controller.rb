679165a2065a22d8f256148564e99b0e4ad4d86c
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-09-07 02:04:24 +0300

    modified controllers file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,26 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   
-  get "/employees" do
-    employees = Employee.all.order(:created_at)
-    employees.to_json
+  get "/attendants" do
+    attendants = Attendant.all.order(:created_at)
+    attendants.to_json
   end
 
-  post "/employees" do
-    employee = Employee.create(salary: params[:salary], username: params[:username],image: params[:image], position: params[:position])
-    employee.to_json
+  post "/attendants" do
+    attendant = Attendant.create(salary: params[:salary], username: params[:username],image: params[:image], job: params[:job])
+    attendant.to_json
   end
 
-  patch "/employees/:id" do
-    employee = Employee.find(params[:id])
-    employee.update(salary: params[:salary], username: params[:username], image: params[:image], position: params[:position])
-    employee.to_json
+  patch "/attendants/:id" do
+    attendant = Attendant.find(params[:id])
+    attendant.update(salary: params[:salary], username: params[:username], image: params[:image], job: params[:job])
+    attendant.to_json
   end
 
-  delete "/employees/:id" do
-    employee = Employee.find(params[:id])
-    employee.destroy
-    employee.to_json
+  delete "/attendants/:id" do
+    attendant = Attendant.find(params[:id])
+    attendant.destroy
+    attendant.to_json
   end
 
 end
