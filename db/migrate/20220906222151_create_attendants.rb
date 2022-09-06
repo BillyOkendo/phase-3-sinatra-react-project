9846cac8fe152f7412cc877025b15561c20e6b5a
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-09-07 01:30:17 +0300

    modified migration file

--- db/migrate/20220906222151_create_attendants.rb
+++ db/migrate/20220906222151_create_attendants.rb
@@ -1,4 +1,10 @@
 class CreateAttendants < ActiveRecord::Migration[6.1]
   def change
+    create_table :attendants do |t|
+      t.string "username"
+      t.string "job"
+      t.string "image"
+      t.string "salary"
+    end
   end
 end
