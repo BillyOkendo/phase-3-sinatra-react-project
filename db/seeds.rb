8f208404e9d1e83923f2b5985bae196b062d9b5f
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-09-07 01:50:12 +0300

    modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ House.destroy_all
 Attendant.destroy_all
 
 puts "Creating houses..."
-finance_section = House.create(name: "finance section")
-IT_section = House.create(name: "IT section")
-surbodinate_staff = House.create(name: "surbodinate staff")
-general_staff = House.create(name: "general staff")
+tims_family = House.create(name: "tims family")
+IT_firm = House.create(name: "IT firm")
+canes_farm = House.create(name: "surbodinate staff")
+jakes_house = House.create(name: "jakes house")
 
 puts "Creating attendants..."
 allan_kush = Attendant.create(username: "Allan Kush", salary:"Salary KSH 50000",job:"Cashier", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmo_Nuzkt2bB5GXUrDQWBeyX6t1TpnSAF0bxYHKjWa&s")
@@ -22,8 +22,8 @@ puts "Creating roles..."
 
 Role.create( salary: "Salary KSH 50000",username: "Allan", house_id:finance_section.id, attendant_id:allan_kush.id)
 Role.create( salary: "Salary KSH 30000",username: "Jane", house_id:general_staff.id, attendant_id:jane_lee.id)
-Role.create(salary: "Salary KSH 20000",username: "Cindy", house_id:surbodinate_staff.id, attendant_id:cindy_amor.id)
-Role.create(salary: "Salary KSH 60000",username: "Cathy", house_id:IT_section.id, attendant_id:cathy_lorna.id)
+Role.create(salary: "Salary KSH 20000",username: "Cindy", house_id:_staff.id, attendant_id:cindy_amor.id)
+Role.create(salary: "Salary KSH 60000",username: "Cathy", house_id:IT_firm.id, attendant_id:cathy_lorna.id)
 
 
 puts "✅ Done seeding!"
